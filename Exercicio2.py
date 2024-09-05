bdb9367200a16e1ab3d368f49fd103413e631c04
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-09-05 02:41:52 -0300

    Update Exercicio2.py

--- Exercicio2.py
+++ Exercicio2.py
@@ -1,23 +1,17 @@
-def fibonacci(numero):
-    # Inicializando os dois primeiros números da sequência
+def fibonacci(nro):
     a, b = 0, 1
     
-    # Se o número informado for 0 ou 1, pertence à sequência
-    if numero == 0 or numero == 1:
+    if nro == 0 or nro == 1:
         return True
     
-    # Loop para calcular a sequência de Fibonacci até encontrar ou ultrapassar o número informado
-    while b < numero:
+    while b < nro:
         a, b = b, a + b
     
-    # Verifica se o número informado é igual ao último valor da sequência calculada
-    return b == numero
+    return b == nro
 
-# Número a ser verificado (pode ser substituído por uma entrada do usuário)
-numero_informado = int(input("Informe um número: "))
+nro_informado = int(input("Informe um número: "))
 
-# Verifica se o número pertence à sequência de Fibonacci
-if fibonacci(numero_informado):
-    print(f"O número {numero_informado} pertence à sequência de Fibonacci.")
+if fibonacci(nro_informado):
+    print(f"O número {nro_informado} pertence à sequência de Fibonacci.")
 else:
-    print(f"O número {numero_informado} NÃO pertence à sequência de Fibonacci.")
+    print(f"O número {nro_informado} NÃO pertence à sequência de Fibonacci.")
